d5d17f3cb7bd6897b5d93e956548a1cfb8ec6646
Shahid Afrid <55510234+shahidafrid366@users.noreply.github.com>
2025-02-03 16:57:34 +0530

    Update update-module-order.ts

--- api/src/courses/modules/update-module-order.ts
+++ api/src/courses/modules/update-module-order.ts
@@ -1,72 +1,69 @@
-
 import { RequestHandler } from "express";
 import { z } from "zod";
 import { db } from "../../common/db";
 import { IdSchema } from "../../common/zod-schemas";
 
-export const updateModuleOrderHandler: RequestHandler = (
+export const updateModuleOrderHandler: RequestHandler = async (
   req,
   res,
   next
-): Promise<void> => {
-  return (async () => {
-    try {
-      const queryParams = await z
-        .object({
-          order: z.union([z.literal("up"), z.literal("down")]),
-        })
-        .parseAsync(req.query);
+) => {
+  try {
+    const queryParams = await z
+      .object({
+        order: z.union([z.literal("up"), z.literal("down")]),
+      })
+      .parseAsync(req.query);
 
-      const moduleId = await IdSchema.parseAsync(req.params.moduleId);
-      const courseId = await IdSchema.parseAsync(req.params.courseId);
+    const moduleId = await IdSchema.parseAsync(req.params.moduleId);
+    const courseId = await IdSchema.parseAsync(req.params.courseId);
 
-      const courseData = await db.course.findUnique({
-        where: {
-          id: courseId,
-        },
-        select: {
-          modulesOrder: true,
-        },
-      });
+    const courseData = await db.course.findUnique({
+      where: {
+        id: courseId,
+      },
+      select: {
+        modulesOrder: true,
+      },
+    });
 
-      if (!courseData) {
-        return res.sendStatus(400);
-      }
-
-      const moduleIndex = courseData?.modulesOrder.findIndex(
-        (i) => i === moduleId
-      );
-
-      if (
-        moduleIndex === -1 ||
-        (moduleIndex === 0 && queryParams.order === "up") || // Can't move first one any more up
-        (moduleIndex === courseData.modulesOrder.length - 1 &&
-          queryParams.order === "down") // Can't move last one down
-      ) {
-        return res.sendStatus(400);
-      }
-
-      const newModulesOrder: number[] = [...courseData.modulesOrder];
-      const indexPositionToSwap =
-        queryParams.order === "up"
-          ? (moduleIndex + courseData.modulesOrder.length - 1) %
-            courseData.modulesOrder.length
-          : (moduleIndex + 1) % courseData.modulesOrder.length;
-      newModulesOrder[moduleIndex] = newModulesOrder[indexPositionToSwap]; // Next one in the cycle
-      newModulesOrder[indexPositionToSwap] = moduleId;
-
-      await db.course.update({
-        where: {
-          id: courseId,
-        },
-        data: {
-          modulesOrder: newModulesOrder,
-        },
-      });
-
-      res.sendStatus(200);
-    } catch (error) {
-      next(error);
+    if (!courseData) {
+      return res.sendStatus(400);
     }
-  })();
+
+    const moduleIndex = courseData?.modulesOrder.findIndex(
+      (i) => i === moduleId
+    );
+
+    if (
+      moduleIndex === -1 ||
+      (moduleIndex === 0 && queryParams.order === "up") || // Can't move first one any more up
+      (moduleIndex === courseData.modulesOrder.length - 1 &&
+        queryParams.order === "down") // Can't move last one down
+    ) {
+      return res.sendStatus(400);
+    }
+
+    const newModulesOrder: number[] = [...courseData.modulesOrder];
+    const indexPositionToSwap =
+      queryParams.order === "up"
+        ? (moduleIndex + courseData.modulesOrder.length - 1) %
+          courseData.modulesOrder.length
+        : (moduleIndex + 1) % courseData.modulesOrder.length;
+    newModulesOrder[moduleIndex] = newModulesOrder[indexPositionToSwap]; // Next one in the cylce
+    newModulesOrder[indexPositionToSwap] = moduleId;
+
+    await db.course.update({
+      where: {
+        id: courseId,
+      },
+      data: {
+        modulesOrder: newModulesOrder,
+      },
+    });
+
+    return res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
 };
