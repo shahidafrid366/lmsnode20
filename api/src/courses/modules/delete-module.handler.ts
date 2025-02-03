2dae675defd2986feeccdde03f6d7474ea544fa8
Shahid Afrid <55510234+shahidafrid366@users.noreply.github.com>
2025-02-03 15:22:02 +0530

    Update delete-module.handler.ts

--- api/src/courses/modules/delete-module.handler.ts
+++ api/src/courses/modules/delete-module.handler.ts
@@ -1,8 +1,27 @@
+// import { RequestHandler } from "express";
+// import { db } from "../../common/db";
+// import { IdSchema } from "../../common/zod-schemas";
+
+// export const deleteModuleHandler: RequestHandler = async (req, res, next) => {
+//   try {
+//     const moduleId = await IdSchema.parseAsync(req.params.moduleId);
+//     await db.module.delete({
+//       where: {
+//         id: moduleId,
+//       },
+//     });
+//     res.sendStatus(200);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 import { RequestHandler } from "express";
 import { db } from "../../common/db";
 import { IdSchema } from "../../common/zod-schemas";
 
-export const deleteModuleHandler: RequestHandler = async (req, res, next) => {
+// Explicitly declare the return type as Promise<void>
+export const deleteModuleHandler: RequestHandler = async (req, res, next): Promise<void> => {
   try {
     const moduleId = await IdSchema.parseAsync(req.params.moduleId);
     await db.module.delete({
