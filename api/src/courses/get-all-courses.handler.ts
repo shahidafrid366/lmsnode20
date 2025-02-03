29e64fa9df64a6cc8d6526ff506a91e31544e88d
Shahid Afrid <55510234+shahidafrid366@users.noreply.github.com>
2025-02-03 15:36:59 +0530

    Update get-all-courses.handler.ts

--- api/src/courses/get-all-courses.handler.ts
+++ api/src/courses/get-all-courses.handler.ts
@@ -1,25 +1,6 @@
-// import { RequestHandler } from "express";
-// import { db } from "../common/db";
-
-// export const getAllCoursesHandler: RequestHandler = async (req, res, next) => {
-//   try {
-//     const data = await db.course.findMany({
-//       select: {
-//         id: true,
-//         title: true,
-//       },
-//     });
-//     res.json(data);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-
 import { RequestHandler } from "express";
 import { db } from "../common/db";
 
-// Handler to fetch all courses
 export const getAllCoursesHandler: RequestHandler = async (req, res, next): Promise<void> => {
   try {
     const data = await db.course.findMany({
