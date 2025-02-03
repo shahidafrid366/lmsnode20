1dd63448010fae74a84274659258f132b76660f1
Shahid Afrid <55510234+shahidafrid366@users.noreply.github.com>
2025-02-03 15:24:39 +0530

    Update get-my-courses.handler.ts

--- api/src/courses/get-my-courses.handler.ts
+++ api/src/courses/get-my-courses.handler.ts
@@ -1,6 +1,35 @@
+// import { RequestHandler } from "express";
+// import { db } from "../common/db";
+
+// export const getEnrolledCourses = async () => {
+//   const data = await db.course.findMany({
+//     where: {
+//       archived: false,
+//     },
+//     select: {
+//       id: true,
+//       title: true,
+//       description: true,
+//       liveLink: true,
+//     },
+//   });
+
+//   return data;
+// };
+
+// export const getMyCoursesHandler: RequestHandler = async (req, res, next) => {
+//   try {
+//     const data = await getEnrolledCourses();
+//     return res.json(data);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 import { RequestHandler } from "express";
 import { db } from "../common/db";
 
+// Async function that retrieves enrolled courses
 export const getEnrolledCourses = async () => {
   const data = await db.course.findMany({
     where: {
@@ -17,11 +46,12 @@ export const getEnrolledCourses = async () => {
   return data;
 };
 
-export const getMyCoursesHandler: RequestHandler = async (req, res, next) => {
+// Explicitly declare the return type as Promise<void>
+export const getMyCoursesHandler: RequestHandler = async (req, res, next): Promise<void> => {
   try {
     const data = await getEnrolledCourses();
-    return res.json(data);
+    return res.json(data); // Sends data as JSON
   } catch (error) {
-    next(error);
+    next(error); // Pass errors to the error handler
   }
 };
