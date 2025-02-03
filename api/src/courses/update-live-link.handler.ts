91ccf90c0d97e738167f8fa81f6ec1768191192c
Shahid Afrid <55510234+shahidafrid366@users.noreply.github.com>
2025-02-03 15:32:50 +0530

    Update update-live-link.handler.ts

--- api/src/courses/update-live-link.handler.ts
+++ api/src/courses/update-live-link.handler.ts
@@ -3,26 +3,6 @@ import { z } from "zod";
 import { db } from "../common/db";
 import { IdSchema } from "../common/zod-schemas";
 
-// export const updateLiveLinkHandler: RequestHandler = async (req, res, next) => {
-//   try {
-//     const courseId = IdSchema.parse(req.params.courseId);
-//     const liveLink = z.string().url().parse(req.body.liveLink);
-
-//     // Update the course in the database
-//     await db.course.update({
-//       where: {
-//         id: courseId,
-//       },
-//       data: {
-//         liveLink,
-//       },
-//     });
-
-//     res.sendStatus(204);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 export const updateLiveLinkHandler: RequestHandler = (req, res, next): Promise<void> => {
   return (async () => {
     try {
