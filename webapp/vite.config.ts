7aef8084c658d99aaa649c196c3cbcd84ff4bd16
Shahid Afrid <55510234+shahidafrid366@users.noreply.github.com>
2025-02-10 22:16:01 +0530

    Update vite.config.ts - changed port to 8080 from 3000

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 8080,
   },
   plugins: [react()],
   test: {
